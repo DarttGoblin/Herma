3231d56efde5e4e924c1ae44e4ce9ee7306528ec
DarttGoblin <yassine.bazgour@gmail.com>
2025-09-07 15:53:09 +0100

    Minor updates

--- Herma/Herma.Scripts/About.js
+++
@@ -1,7 +0,0 @@
-const benefit_container = document.querySelectorAll('.benefit-container'); 
-
-benefit_container.forEach(container => {
-    container.onclick = function() {
-        window.open('https://www.kaggle.com/datasets/andrewmvd/divorce-prediction/data', '_blank');
-    }
-});
