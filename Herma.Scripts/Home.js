986daa21d6b125883ce1ceae49bad3b4721729b6
DarttGoblin <yassine.bazgour@gmail.com>
2025-09-07 18:04:34 +0100

    Added demo button

--- Herma.Scripts/Home.js
+++ Herma.Scripts/Home.js
@@ -1,5 +1,6 @@
 const header_start_button = document.querySelector('.header-start-button');
 const header_github_button = document.querySelector('.header-github-button');
+const header_demo_button = document.querySelector('.header-demo-button');
 
 header_start_button.onclick = function() {
     section[1].scrollIntoView({behavior: 'smooth'});
@@ -7,4 +8,8 @@ header_start_button.onclick = function() {
 
 header_github_button.onclick = function() {
     window.open('https://github.com/DarttGoblin/Herma_server', '_blank');
+}
+
+header_demo_button.onclick = function() {
+    window.open('https://drive.google.com/file/d/1Xk67mQI4DaenoYzZ7zZE7ugApZr83jhW/view?usp=sharing', '_blank');
 }
